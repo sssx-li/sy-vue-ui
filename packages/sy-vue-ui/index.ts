d7421f01d19ba4b0a7cf80df497c1bcc7b887a22
lizhiquan <842048365@qq.com>
2023-06-06 10:43:29 +0800

    perf: 代码目录优化

--- packages/sy-vue-ui/index.ts
+++
@@ -1,6 +0,0 @@
-import installer from './defaults';
-
-export * from '@sy-vue-ui/components';
-export const install = installer.install;
-
-export default installer;
--- typing/component.d.ts
+++ typing/component.d.ts
@@ -1,8 +1,8 @@
 // GlobalComponents for Volar
-import * as components from "../packages/components";
-declare module "@vue/runtime-core" {
+import * as components from '../packages/components/src';
+declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    SyButton: (typeof components)["SyButton"];
+    SyButton: (typeof components)['SyButton'];
   }
 }
 
